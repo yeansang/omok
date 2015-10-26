b543a9c59ec6983da43e6d16d0bdfb7109a6cbc7
Yeansang Ahn <ysan1991@gmail.com>
2015-10-26 15:25:11 +0900

    frame interface set

--- src/netGame/gameFrame.java
+++ src/netGame/gameFrame.java
@@ -26,7 +26,16 @@ class chessL implements ActionListener {
 
 
 public class gameFrame extends JFrame {
-	public gameFrame() {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	gameEngine in;
+	public gameFrame(gameEngine e) {
+		in = e;
+		int X = e.paneX;
+		int Y = e.paneY;
 		setTitle("오목 판");
 		setSize(500,500);
 		setVisible(true);
@@ -34,12 +43,29 @@ public class gameFrame extends JFrame {
 		
 		Container con = getContentPane();
 		
-		con.setLayout(new GridLayout(8,8));
+		con.setLayout(new GridLayout(X,Y));
+		
 		
 	}
-	public void init(){
+	//돌 표시
+	public void view(){
+		for(int i=0;i<in.paneX;i++){
+			for(int j=0;j<in.paneY;j++){
+				if((in.pane[i][j].visable==true)&&(in.pane[i][j].color==true)) ;//검은돌 놓기
+				
+				if((in.pane[i][j].visable==true)&&(in.pane[i][j].color==false)) ;//흰돌 놓기
+			}
+		}
+		repaint();
+	}
+	public Piece userOut(){
+		//사용자 입력 반환
+		int x = 0;//x축
+		int y = 0;//y축
+		return (new Piece(x,y,true));
 		
 	}
+	
 	
 
 }
